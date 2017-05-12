969531205ec859f30ac59129ba529db45238a56f
creeperyang <yangcreeper@hotmail.com>
2017-05-12 14:10:04 +0800

    feat: add docs

---
+++ docs/_source/app.js
@@ -0,0 +1,39 @@
+import STYLE from './app.css'
+
+var $hanzi = document.querySelector('#hanzi')
+var $pinyin = document.querySelector('#pinyin')
+
+if (!Pinyin.isSupported()) {
+  $hanzi.remove()
+  $pinyin.remove()
+  document.querySelector('#not-support').className = ''
+} else {
+  $hanzi.onkeyup = onChange
+  $hanzi.onchange = onChange
+  $hanzi.oncopy = onChange
+}
+
+function onChange () {
+  var value = $hanzi.value
+  var pinyin = ''
+  if (value) {
+    var tokens = Pinyin.parse($hanzi.value)
+    var lastToken
+    tokens.forEach((function(v, i) {
+      if (v.type === 2) {
+        pinyin += (pinyin && lastToken.target !== ' ') ? (' ' + format(v.target)) : format(v.target)
+      } else {
+        pinyin += v.target
+      }
+      lastToken = v
+    }))
+  }
+  $pinyin.textContent = pinyin
+}
+
+function format(str) {
+  if (str) {
+    return str.toLowerCase()
+  }
+  return ''
+}
---
+++ docs/app.js
@@ -0,0 +1,127 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _app = __webpack_require__(0);
+
+var _app2 = _interopRequireDefault(_app);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var $hanzi = document.querySelector('#hanzi');
+var $pinyin = document.querySelector('#pinyin');
+
+if (!Pinyin.isSupported()) {
+  $hanzi.remove();
+  $pinyin.remove();
+  document.querySelector('#not-support').className = '';
+} else {
+  $hanzi.onkeyup = onChange;
+  $hanzi.onchange = onChange;
+  $hanzi.oncopy = onChange;
+}
+
+function onChange() {
+  var value = $hanzi.value;
+  var pinyin = '';
+  if (value) {
+    var tokens = Pinyin.parse($hanzi.value);
+    var lastToken;
+    tokens.forEach(function (v, i) {
+      if (v.type === 2) {
+        pinyin += pinyin && lastToken.target !== ' ' ? ' ' + format(v.target) : format(v.target);
+      } else {
+        pinyin += v.target;
+      }
+      lastToken = v;
+    });
+  }
+  $pinyin.textContent = pinyin;
+}
+
+function format(str) {
+  if (str) {
+    return str.toLowerCase();
+  }
+  return '';
+}
+
+/***/ })
+/******/ ]);
